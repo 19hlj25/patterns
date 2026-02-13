947f5f70630bfbf76c598bd1011433373710a6ee
Angela Suell <19hlj25@proton.me>
2026-02-12 18:53:16 -0700

    two done

--- patterns/accumulator.js
+++ patterns/accumulator.js
@@ -22,7 +22,14 @@ export function sumToN(n) {
  * @returns `1` if n is 0
  */
 export function factorial(n) {
-  // TODO
+  if (typeof n !== "number") return NaN;
+  if (n < 0) return undefined;
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = 1; i <= n; i++) {
+    product = i * product;
+  }
+  return product;
 }
 
 /**
@@ -32,7 +39,13 @@ export function factorial(n) {
  * @returns `[]` if n is 0 or negative
  */
 export function buildNArray(n) {
-  // TODO
+  if (typeof n !== "number") return null;
+  if (n < 0) return [];
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    arr.push(i);
+  }
+  return arr;
 }
 
 /**
@@ -40,7 +53,13 @@ export function buildNArray(n) {
  * @returns {string} the longest string in `strings`
  */
 export function getLongestString(strings) {
-  // TODO
+  let LongestString = "";
+  for (let i = 0; i < strings.length; i++) {
+    if (strings[i].length > longest.length) {
+      longest = strings[i];
+    }
+  }
+  return longest;
 }
 
 /**
@@ -48,7 +67,13 @@ export function getLongestString(strings) {
  * @returns {number} the number of students present
  */
 export function countPresent(attendance) {
-  // TODO
+  let count = 0;
+  for (let i = 0; i < attendance.length; i++) {
+    if (attendance[i] === true) {
+      count++;
+    }
+  }
+  return count;
 }
 
 /**
@@ -62,5 +87,22 @@ export function countPresent(attendance) {
  * @returns `null` if `dna` is not a string
  */
 export function complementDNA(dna) {
-  // TODO
+  if (typeof dna !== "string") return null;
+  let foundDna = "";
+  for (let i = 0; i < dna.length; i++) {
+    const indexDna = dna[i];
+    if (indexDna === "A") {
+      return "T";
+    }
+    if (indexDna === "T") {
+      return "C";
+    }
+    if (indexDna === "C") {
+      return "G";
+    }
+    if (indexDna === "G") {
+      return "C";
+    }
+  }
+  return indexDna;
 }
